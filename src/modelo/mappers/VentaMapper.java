fff9f30ff8ab574210d0880e4668d8456ad8f7f2
Joches29 <jochaveshe@est.utn.ac.cr>
2025-12-07 11:45:46 -0600

    Cambios en tipo de datos al trabajar con Producto y Clientes de Marco

--- src/modelo/mappers/VentaMapper.java
+++ src/modelo/mappers/VentaMapper.java
@@ -4,10 +4,13 @@
  */
 package modelo.mappers;
 
+import modelo.Cliente;
 import modelo.DetalleVenta;
+import modelo.EstadoVentaEnum;
 import modelo.Producto;
 import modelo.Venta;
 import modelo.dtos.DetalleVentaDTO;
+import modelo.dtos.VentaDTO;
 
 /**
  *
@@ -15,31 +18,31 @@ import modelo.dtos.DetalleVentaDTO;
  */
 public class VentaMapper {
      
-    public static DetalleVenta toEntity(DetalleVentaDTO dto, Venta venta, Producto producto) {
+    
+    
+    public static Venta toEntity(VentaDTO dto, Cliente cliente) {
         if (dto == null){
              throw new IllegalArgumentException("El dto no puede estar vacio");
         }
         
-        return new DetalleVenta(
+        return new Venta(
             dto.getId(),
-            venta, 
-            producto, 
-            dto.getCantidad(),
-            dto.getPrecioUnitario()
+            cliente,
+            dto.getFecha(),
+            dto.getEstado() != null ? EstadoVentaEnum.fromString(dto.getEstado()) : null
         );
     }
 
-    public static DetalleVentaDTO toDTO(DetalleVenta entity) {
-        if (entity == null){
-             throw new IllegalArgumentException("El detalle de venta no puede estar vacio");
+    public static VentaDTO toDTO(Venta entity) {
+        if (entity == null) {
+             throw new IllegalArgumentException("La venta no puede estar vacia");
         }
         
-        return new DetalleVentaDTO(
+        return new VentaDTO(
             entity.getId(),
-            entity. getVenta() != null ? entity.getVenta().getId() : null,     
-            entity.getProducto() != null ?  entity.getProducto().getCodigo() : null,
-            entity.getCantidad(),
-            entity.getPrecioUnitario()
+            entity.getCliente() != null ? entity.getCliente().getCedula() : null, 
+            entity.getFecha(),
+            entity.getEstado() != null ? entity.getEstado().name() : null
         );
     }
 }
